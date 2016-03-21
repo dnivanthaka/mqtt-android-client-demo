b6008f045f83fab45e0e51ccb6ec617d5241d9cd
dnivanthaka <nivanthaka@gmail.com>
2016-03-21 23:22:29 +0530

    Updated MainActivity class with subscribe and unsubscribe methods

--- app/src/main/java/com/nivanthaka/mqttclienttest/MainActivity.java
+++ app/src/main/java/com/nivanthaka/mqttclienttest/MainActivity.java
@@ -14,12 +14,14 @@ import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
 import org.eclipse.paho.client.mqttv3.IMqttToken;
 import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 
 //http://www.hivemq.com/blog/mqtt-client-library-enyclopedia-paho-android-service
 public class MainActivity extends AppCompatActivity {
 
     private final String TAG = "MQTT_CLIENT";
+    private MqttAndroidClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,19 +64,26 @@ public class MainActivity extends AppCompatActivity {
 
     public void clientConnection(View view){
         Log.d(TAG, "onClientConnection");
+        //tcp://broker.hivemq.com:1883
 
         String clientId = MqttClient.generateClientId();
-        MqttAndroidClient client =
-                new MqttAndroidClient(this.getApplicationContext(), "tcp://broker.hivemq.com:1883",
+        client = new MqttAndroidClient(this.getApplicationContext(), "tcp://broker.hivemq.com:1883",
                         clientId);
 
+        MqttConnectOptions options = new MqttConnectOptions();
+        //Last will message
+        String topic = "users/last/will";
+        byte[] payload = "some payload".getBytes();
+        options.setWill(topic, payload ,1,false);
+
         try {
-            IMqttToken token = client.connect();
+            IMqttToken token = client.connect(options);
             token.setActionCallback(new IMqttActionListener() {
                 @Override
                 public void onSuccess(IMqttToken asyncActionToken) {
                     // We are connected
                     Log.d(TAG, "onSuccess");
+
                 }
 
                 @Override
@@ -82,6 +91,54 @@ public class MainActivity extends AppCompatActivity {
                     // Something went wrong e.g. connection timeout or firewall problems
                     Log.d(TAG, "onFailure");
 
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void mqttSubscribe(){
+        String topic = "foo/bar";
+        int qos = 1;
+
+        try {
+            IMqttToken subToken = client.subscribe(topic, qos);
+            subToken.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // The message was published
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken,
+                                      Throwable exception) {
+                    // The subscription could not be performed, maybe the user was not
+                    // authorized to subscribe on the specified topic e.g. using wildcards
+
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void mqttUnsubscribe(){
+        final String topic = "foo/bar";
+        try {
+            IMqttToken unsubToken = client.unsubscribe(topic);
+            unsubToken.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // The subscription could successfully be removed from the client
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken,
+                                      Throwable exception) {
+                    // some error occurred, this is very unlikely as even if the client
+                    // did not had a subscription to the topic the unsubscribe action
+                    // will be successfully
                 }
             });
         } catch (MqttException e) {
